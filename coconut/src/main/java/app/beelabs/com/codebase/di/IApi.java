80e0911ee086b179205e9368d6dd5059ec7bee51
asywalulfikri <asywalul.fikri@ottodigital.id>
2022-08-08 14:10:48 +0700

    update

--- coconut/src/main/java/app/beelabs/com/codebase/di/IApi.java
+++ coconut/src/main/java/app/beelabs/com/codebase/di/IApi.java
@@ -3,13 +3,7 @@ package app.beelabs.com.codebase.di;
 
 import android.content.Context;
 
-import com.chuckerteam.chucker.api.ChuckerInterceptor;
-
 import okhttp3.Interceptor;
-
-/**
- * Created by arysuryawan on 8/21/17.
- */
 
 public interface IApi {
     Object initApiService(String apiDomain, boolean allowUntrusted, Class<IApiService> clazz, int timeout, boolean enableLoggingHttp, String PedePublicKeyRSA);
